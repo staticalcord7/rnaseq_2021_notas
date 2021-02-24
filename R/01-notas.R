6d0cc699cf3e04f82e818f7ba58fccf788bf8d15
staticalcord7 <staticalcord7.jr@gmail.com>
2021-02-24 09:26:17 -0600

    aa

--- R/01-notas.R
+++ R/01-notas.R
@@ -1,1 +1,21 @@
-primeras notas prueba
+library("sessioninfo")
+library("here")
+library("ggplot2")
+
+## Hello world
+print("Soy Joshua")
+
+## Crear directorio para las figuras
+dir.create(here::here("figuras"), showWarnings = FALSE)
+
+## Hacer una imagen de ejemplo
+pdf(here::here("figuras", "mtcars_gear_vs_mpg.pdf"),
+    useDingbats = FALSE
+)
+ggplot(mtcars, aes(group = gear, y = mpg)) +
+  geom_boxplot()
+dev.off()
+
+## Para reproducir mi código
+options(width = 120)
+sessioninfo::session_info()
